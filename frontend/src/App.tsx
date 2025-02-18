c1d41354f87f98c81c40b35ef820498f19a2427f
PriyaNayak20 <priya.n@webkorps.com>
2025-02-19 01:47:16 +0530

    added files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>FashionFusion</header>
+        <main></main>
+        <footer>All the rights</footer>
+      </div>
+    </>
+  )
+}
+
+export default App
