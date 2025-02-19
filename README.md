84f99c35fbece0f8d174024b5a53746c03254d3c
PriyaNayak20 <Priya.n@webkorps.com>
2025-02-19 20:24:17 +0530

    added product list

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TypeScript MERN WearEase
+
+1. List Products
+1. create Product type
+1. create product array
+1. add product images
+1. render products
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { sampleProdcts } from './data'
 
 function App() {
   return (
     <>
       <div>
         <header>FashionFusion</header>
-        <main></main>
+        <main>
+          <ul>
+            {sampleProdcts.map((products) => (
+              <li key={products.slug}>
+                <h2>{products.name}</h2>
+                <img
+                  src={products.image}
+                  alt={products.name}
+                  className="product-image"
+                />
+                <p>${products.price}</p>
+              </li>
+            ))}
+          </ul>
+        </main>
         <footer>All the rights</footer>
       </div>
     </>
